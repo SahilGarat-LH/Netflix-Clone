680920d0c1b291bb3e9f7435aedd240f174fcd6d
SAHIL GARAT <sahilgarat@SAHILs-MacBook-Air.local>
2021-06-26 00:27:27 +0530

    app built

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA_WuEe7U17JUjtceTNxGvjc7rhwkNtMUU",
+    authDomain: "netflix-27bf2.firebaseapp.com",
+    projectId: "netflix-27bf2",
+    storageBucket: "netflix-27bf2.appspot.com",
+    messagingSenderId: "493814342075",
+    appId: "1:493814342075:web:dbdc82ed79b5218590249e"
+  };
